b3478dbe0dd5ccfd3b39dc1c1c3040af6ec284e3
Chris <chris@cypheros.co>
2017-10-20 13:31:38 -0400

    aoscp/interfaces: update makefile vars for aoscp
    Change-Id: Ic09bd88b0e22ae83f893defe2ef3fa2777037de8

--- update-makefiles.sh
+++ update-makefiles.sh
@@ -3,6 +3,6 @@
 source system/tools/hidl/update-makefiles-helper.sh
 
 do_makefiles_update \
-  "vendor.lineage:hardware/lineage/interfaces" \
+  "vendor.aoscp:hardware/aoscp/interfaces" \
   "android.hardware:hardware/interfaces" \
   "android.hidl:system/libhidl/transport"
